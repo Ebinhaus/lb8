6d1afc7fbb8bad7d15736829f9c683f6570baeb1
Ebinhaus <stepa.r67123456789@gmail.com>
2023-12-06 10:15:05 +0300

    w

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Link } from "react-router-dom";
+import './index.css'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Myform from "./pages/MyForm";
+import Product from "./pages/Product";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ return (
+  <BrowserRouter>
+   <div className="list">
+     <h1>Учебное SPA</h1>
+     <ul className="header">
+      <li><Link to="/">Главная</Link></li>
+      <li><Link to="about">О нас</Link></li>
+      <li><Link to="contact">Контакты</Link></li>
+      <li><Link to="frm">Форма</Link></li>
+      <li><Link to="prod">Продукты</Link></li>
+    </ul>
     </div>
-  );
+    <Routes>
+    <Route path="/" element={<Home/>} />
+    <Route path="about" element={<About/>} />
+    <Route path="contact" element={<Contact/>} />
+    <Route path="frm" element={<Myform/>}/>
+    <Route path="prod" element={<Product/>}></Route>
+   </Routes>
+  </BrowserRouter>
+ );
 }
 
 export default App;
